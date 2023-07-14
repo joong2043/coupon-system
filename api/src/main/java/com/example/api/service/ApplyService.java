9f9dcc831e9c543dc89afd7498cd22b96ec95ab8
joonghyun <80939285+joong2043@users.noreply.github.com>
2023-07-14 20:51:22 +0900

    feat : Kafka를 통해 쿠폰 저장 서비스 구현

--- api/src/main/java/com/example/api/service/ApplyService.java
+++ api/src/main/java/com/example/api/service/ApplyService.java
@@ -1,6 +1,7 @@
 package com.example.api.service;
 
-import com.example.api.domain.Coupon;
+import com.example.api.producer.CouponCreateProducer;
+import com.example.api.repository.AppliedUserRepository;
 import com.example.api.repository.CouponCountRepository;
 import com.example.api.repository.CouponRepository;
 import org.springframework.stereotype.Service;
@@ -12,12 +13,23 @@ public class ApplyService {
 
     private final CouponCountRepository couponCountRepository;
 
-    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository){
+    private final CouponCreateProducer couponCreateProducer;
+    private final AppliedUserRepository appliedUserRepository;
+
+    public ApplyService(CouponRepository couponRepository, CouponCountRepository couponCountRepository, CouponCreateProducer couponCreateProducer, AppliedUserRepository appliedUserRepository){
         this.couponRepository = couponRepository;
         this.couponCountRepository = couponCountRepository;
+        this.couponCreateProducer = couponCreateProducer;
+        this.appliedUserRepository = appliedUserRepository;
     }
 
     public void apply(Long userId){
+        Long apply = appliedUserRepository.add(userId);
+
+        if (apply != 1) {
+            return;
+        }
+
         // redis -> single thread -> race condition 해결 가능
         long count = couponCountRepository.increment(); // 쿠폰 개수 -> 핵심키 -> 정합성만 유지
 
@@ -25,6 +37,6 @@ public class ApplyService {
             return;
         }
 
-        couponRepository.save(new Coupon(userId));
+        couponCreateProducer.create(userId);
     }
 }
--- api/src/test/java/com/example/api/service/ApplyServiceTest.java
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -54,8 +54,42 @@ public class ApplyServiceTest {
 
         latch.await();
 
+        Thread.sleep(10 * 1000);
+
         long count = couponRepository.count();
 
         assertThat(count).isEqualTo(100);
+    }
+
+    @Test
+    public void 한명당_한개의쿠폰만_발급() throws InterruptedException {
+        int threadCount = 1000;
+
+        // 병렬작업을 간단하게 할 수 있게 도와주는 자바 api
+        ExecutorService executorService = Executors.newFixedThreadPool(32); // 32개의 스레드를 가진 스레드풀 생성
+
+        // 다른 스레드에서 수행하는 작업을 기다리도록 도와줌
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        // 1000개 요청 반복문
+        for (int i = 0; i < threadCount; i++){
+            long userId = i;
+
+            executorService.submit(() -> {
+                try {
+                    applyService.apply(1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Thread.sleep(10 * 1000);
+
+        long count = couponRepository.count();
+
+        assertThat(count).isEqualTo(1);
     }
 }
